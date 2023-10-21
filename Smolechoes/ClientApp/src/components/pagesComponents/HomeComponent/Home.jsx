ea7023cebe92ad7d5e5c63975c0987f063a3491f
XELLAZH <zhuchkovsasha2004@yandex.ru>
2023-10-22 01:02:12 +0300

    Started adaptive design creation

--- Smolechoes/ClientApp/src/components/pagesComponents/HomeComponent/Home.jsx
+++ Smolechoes/ClientApp/src/components/pagesComponents/HomeComponent/Home.jsx
@@ -3,22 +3,21 @@ import './Home.css';
 import HeaderComponent from "../../structureElements/Header/HeaderComponent";
 
 export default function Home() {
-
+    const width = window.innerWidth;
     return(
         <div className="main-container">
             <HeaderComponent />
             <main>
                 <img src="./images/home_page_background_image.png" alt="home-page-background-image" className="background-image" />
-
                 <div className="content">
                     <h1 className="heading">Экскурсии голосами минувших эпох</h1>
-                    <p className="info">Городская культура и древние традиции Смоленска в атмосфере соответствующего исторического времени – это проект «Smolechoes». Для Вас созданы экскурсионные маршруты по самым знаменитым достопримечательностям с аудиосопровождением в стилистиках древнерусского периода, Смутного времени, Советской эпохи.</p>
-                    <div className="to-exc-btn" onClick={() => {window.location.href = "/excursions"}}>
-                        К экскурсиям
-                        <img className="arrow-icon" src="./images/forward_arrow_icon.svg" alt="to-excursions-btn-arrow-icon" />
-                    </div>
+                    {/* <p className="info">Городская культура и древние традиции Смоленска в атмосфере соответствующего исторического времени – это проект «Smolechoes». Для Вас созданы экскурсионные маршруты по самым знаменитым достопримечательностям с аудиосопровождением в стилистиках древнерусского периода, Смутного времени, Советской эпохи.</p> */}
+                    {/* <div className="to-exc-btn" onClick={() => {window.location.href = "/excursions"}}> */}
+                        {/* К экскурсиям  */}
+                        {/* <img className="arrow-icon" src="./images/forward_arrow_icon.svg" alt="to-excursions-btn-arrow-icon" /> */}
+                    {/* </div> */}
                 </div>
-
+                    
             </main>
         </div>
     )
--- Smolechoes/ClientApp/src/components/structureElements/Header/HeaderComponent.jsx
+++ Smolechoes/ClientApp/src/components/structureElements/Header/HeaderComponent.jsx
@@ -5,9 +5,11 @@ export default function HeaderComponent() {
     return(
         <header>
             <div className="header-container">
-                <a href="/" className="logo-container">
-                    <img src="./images/Logo_bordered.svg" alt="Logo icon" />
-                    <span className="logo-title">Smolechoes</span>
+                <a href="/" className="logo-link">
+                    <div className="logo-container">
+                        <img className="logo-icon" src="./images/Logo_bordered.svg" alt="Logo icon" />
+                        <span className="logo-title">Smolechoes</span>
+                    </div>
                 </a>
             </div>
         </header>
