d1429793106cf3ec7860d6f109d926c364beebbf
XELLAZH <zhuchkovsasha2004@yandex.ru>
2023-10-28 20:07:23 +0300

    Excursions page done!

--- Smolechoes/ClientApp/src/App.js
+++ Smolechoes/ClientApp/src/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import './styles/App.css'
+import Home from './components/pagesComponents/HomeComponent/Home';
+import Excursions from './components/pagesComponents/ExcursionsComponent/Excursions';
 
 export default class App extends Component {
   static displayName = App.name;
 
   render() {
+    console.log(window.innerWidth);
+
     return (
       <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
+          <Route path="/" element={<Home/>}/>
+          <Route path="/excursions" element={<Excursions/>} />
       </Routes>
     );
   }
