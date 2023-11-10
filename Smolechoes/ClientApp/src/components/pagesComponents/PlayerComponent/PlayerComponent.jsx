ba29ec2081cec8715b9ba3c1e6f11aceda55447c
XELLAZH <zhuchkovsasha2004@yandex.ru>
2023-11-10 12:46:08 +0300

    Player layout complete

--- Smolechoes/ClientApp/src/components/pagesComponents/PlayerComponent/PlayerComponent.jsx
+++ Smolechoes/ClientApp/src/components/pagesComponents/PlayerComponent/PlayerComponent.jsx
@@ -9,7 +9,24 @@ export default function PlayerComponent() {
             <div className="player-page-content">
                 <div className="player-block">
                     <div className="player-map"></div>
-                    <div className="player-controls"></div>
+                    <div className="player-controls">
+                        <span className="point-name">Крепостная стена</span>
+                        <div className="track-line">
+                            <div className="line"></div>
+                            <span className="point-number">0</span>
+                        </div>
+                        <div className="btns">
+                            <div className="prev-point-btn">
+                                <img src="./images/prevPointBtn.svg" alt="previous point button" className="prev-btn-icon"/>
+                            </div>
+                            <div className="pause-btn">
+                                <img src="./images/pauseBtn.svg" alt="pause audio button" className="pauseBtn"/>
+                            </div>
+                            <div className="next-point-btn">
+                                <img src="./images/nextPointBtn.svg" alt="next point button" className="next-btn-icon"/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
